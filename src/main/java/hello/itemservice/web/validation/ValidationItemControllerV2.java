bd169f00980c23227805945f32a6ef0c31d78d18
LeeJeongGi <apple4rhk@naver.com>
2022-04-15 22:09:57 +0900

    refactor : BindingResult 기능3 - message.properties 활용하여 처리

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -129,16 +129,16 @@ public class ValidationItemControllerV2 {
 
         //검증 로직
         if(!StringUtils.hasText(item.getItemName())) {
-            bindingResult.addError(new FieldError("item", "itemName", item.getItemName(), false, new String[]{"required.item.itemName"},null, "상품 이름은 필수 입니다."));
+            bindingResult.addError(new FieldError("item", "itemName", item.getItemName(), false, new String[]{"required.item.itemName"},null, null));
         }
 
         if(item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
-            bindingResult.addError(new FieldError("item", "price", item.getPrice(), false, new String[]{"range.item.price"}, new Object[]{1000, 10000},  "가격은 1,000 ~ 1,000,000 까지 허용합니다."));
+            bindingResult.addError(new FieldError("item", "price", item.getPrice(), false, new String[]{"range.item.price"}, new Object[]{1000, 1000000},  null));
         }
 
         if(item.getQuantity() == null || item.getQuantity() >= 9999) {
             bindingResult.addError(new FieldError("item", "quantity", item.getQuantity()
-                    , false, new String[]{"max.item.quantity"}, new Object[]{9999}, "수량은 최대 9999 까지 허용합니다."));
+                    , false, new String[]{"max.item.quantity"}, new Object[]{9999}, null));
         }
 
         //특정 필드가 아닌 복합 룰 검증
@@ -146,7 +146,7 @@ public class ValidationItemControllerV2 {
             int resultPrice = item.getPrice() * item.getQuantity();
 
             if(resultPrice < 10000) {
-                bindingResult.addError(new ObjectError("item", new String[]{"totalPriceMin"}, new Object[]{10000, resultPrice}, "가격 * 수량의 합은 10000원 이상이어야 합니다. 현재 값 : " + resultPrice));
+                bindingResult.addError(new ObjectError("item", new String[]{"totalPriceMin"}, new Object[]{10000, resultPrice}, null));
             }
         }
 
